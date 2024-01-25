085261ee0e744af8170ee3381305b03ced0097ec
Zach Neill <neillz@berea.edu>
2024-01-25 18:28:33 -0500

    pylint 9.46

--- app/models/objects/post.py
+++ app/models/objects/post.py
@@ -1,5 +1,5 @@
 """This file contains the Post object for the database"""
-from sqlalchemy import func
+from datetime import datetime
 
 from database import db
 
@@ -9,7 +9,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False, default="Untitled Post")
     content = db.Column(db.String(1000), nullable=False)
-    date = db.Column(db.DateTime(timezone=True), nullable=False, default=func.now())
+    date = db.Column(db.DateTime(timezone=True), nullable=False, default=datetime.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
 
     def __repr__(self):
