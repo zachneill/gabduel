75024a13fe646933c0b325f23c8fe4754b0037aa
Zach Neill <neillz@berea.edu>
2024-02-08 03:25:19 -0500

    re added daisyui and tailwind

--- frontend/src/components/Welcome/Welcome.tsx
+++ frontend/src/components/Welcome/Welcome.tsx
@@ -4,6 +4,7 @@ import classes from './Welcome.module.css';
 export function Welcome() {
   return (
     <>
+
       <Title className={classes.title} ta="center" mt={100}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -1,11 +1,13 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import {Welcome} from '../components/Welcome/Welcome';
+import {ColorSchemeToggle} from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import Navbar from "@/components/Navbar/Navbar";
 
 export function HomePage() {
-  return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
-  );
-}
+    return (
+        <>
+            <Navbar />
+            <Welcome />
+            <ColorSchemeToggle/>
+            </>
+            );
+            }
